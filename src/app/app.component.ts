2d3e363e707e0c524d3a5792b3356fb53ab22a16
Ljojua15 <jojualasha15@gmail.com>
2023-11-09 17:34:40 +0400

    addimages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -56,16 +56,8 @@ export class AppComponent implements AfterViewInit {
   }
 
   addCourse() {
-    this.course.push({
-      id: 1,
-      description: 'Angular Core Deep Dive',
-      iconUrl:
-        'https://s3-us-west-1.amazonaws.com/angular-university/course-images/angular-core-in-depth-small.png',
-      longDescription:
-        'A detailed walk-through of the most important part of Angular - the Core and Common modules',
-      lessonsCount: 10,
-      category: 'INTERMEDIATE',
-    });
+    const p = this.cards?.get(1)?.nativeElement.querySelector('p');
+    p.style.color = 'blue';
   }
 
   toSelect(test: Course) {
